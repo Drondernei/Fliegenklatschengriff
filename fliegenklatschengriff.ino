48a2b8a2242a6010fbb14bc7e365d3be4345a4f9
Markus <markus@drondernei.de>
2021-05-04 16:31:44 +0200

    added bib
    blinking lights + sound added

--- fliegenklatschengriff.ino
+++ fliegenklatschengriff.ino
@@ -1,20 +1,51 @@
+#include "pitches.h"
+
+int blueLED = 10;
+int redLED = 9;
+int passiveBuzzer = 8;
+int tiltSwitch = 2;
+
+int ledArray[] = {blueLED, redLED};
+
+int sirene[] = {NOTE_E3, 0, NOTE_E5, 0};
+int toneDuration = 500;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(blueLED, OUTPUT);
+  pinMode(redLED, OUTPUT);
+  pinMode(passiveBuzzer, OUTPUT);
+  pinMode(tiltSwitch, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(digitalRead(tiltSwitch) == HIGH)
+  {
+    for(int i = 0; i<4; i++){
+      digitalWrite(ledArray[i%2], HIGH);
+      digitalWrite(ledArray[abs(i%2-1)], LOW);
+      tone(passiveBuzzer, sirene[i], toneDuration);
+      delay(500);
+    }
+  }else{
+    digitalWrite(blueLED, LOW);
+    digitalWrite(redLED, LOW);
+    tone(passiveBuzzer, 0, toneDuration);
+  }
 
+
+
+  delay(10);
 }
 
 /*
- * /******************************************
+ ******************************************
  *Website: www.elegoo.com
  * 
  *Time:2018.1.23
  *
- ******************************************/
+ *****************************************
 
 #include "pitches.h"
  
