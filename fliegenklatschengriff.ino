386cfe9f546da57a8f246d78c05ce3b5bddfba60
Markus <markus@drondernei.de>
2021-05-06 11:27:41 +0200

    corrected joystick sw pin to 0

--- fliegenklatschengriff.ino
+++ fliegenklatschengriff.ino
@@ -1,9 +1,15 @@
+//Used for buzzer sound
 #include "pitches.h"
 
+//LEDs for flash light
 int blueLED = 10;
 int redLED = 9;
+//Buzzer as sound module
 int passiveBuzzer = 8;
-int tiltSwitch = 2;
+//Joystick as control unit
+int xAxis = A0;
+int yAxis = A1;
+int zButton = 0;
 
 int ledArray[] = {blueLED, redLED};
 
@@ -15,59 +21,52 @@ void setup() {
   pinMode(blueLED, OUTPUT);
   pinMode(redLED, OUTPUT);
   pinMode(passiveBuzzer, OUTPUT);
-  pinMode(tiltSwitch, INPUT);
+  pinMode(xAxis, INPUT);
+  pinMode(yAxis, INPUT);
+  pinMode(zButton, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(tiltSwitch) == HIGH)
+
+  //The Joysticks Y-Axis means X-Axis on the real "Hardware" due to installation method; priority on this event
+
+  //yAxis controls sirene incl. flashing lights
+  if (analogRead(yAxis) < 50 || analogRead(yAxis) > 950)
   {
-    for(int i = 0; i<4; i++){
-      digitalWrite(ledArray[i%2], HIGH);
-      digitalWrite(ledArray[abs(i%2-1)], LOW);
+    for (int i = 0; i < 4; i++) {
+      digitalWrite(ledArray[i % 2], HIGH);
+      digitalWrite(ledArray[abs(i % 2 - 1)], LOW);
       tone(passiveBuzzer, sirene[i], toneDuration);
+
       delay(500);
     }
-  }else{
+    
+  //xAxis controls blue and red LED, depends on direction
+  } else if (analogRead(xAxis) < 50) {
+    digitalWrite(blueLED, HIGH);
+    delay(350);
     digitalWrite(blueLED, LOW);
+    delay(350);
+  } else if (analogRead(xAxis) > 950) {
+    digitalWrite(redLED, HIGH);
+    delay(350);
     digitalWrite(redLED, LOW);
+    delay(350);
+    
+  //Pressing the Joystick (zButton) leads to "meep meep"
+  }else if (digitalRead(zButton) == LOW) {
+    Serial.println(digitalRead(zButton));
+    tone(passiveBuzzer, NOTE_E5, toneDuration);
+    delay(500);
     tone(passiveBuzzer, 0, toneDuration);
+    delay(250);
+    tone(passiveBuzzer, NOTE_E5, toneDuration);
+    delay(500);
+    tone(passiveBuzzer, 0, toneDuration);
+    delay(500);
   }
-
-
-
-  delay(10);
+  //reset afterwards
+  digitalWrite(blueLED, LOW);
+  digitalWrite(redLED, LOW);
 }
-
-/*
- ******************************************
- *Website: www.elegoo.com
- * 
- *Time:2018.1.23
- *
- *****************************************
-
-#include "pitches.h"
- 
-// notes in the melody:
-int melody[] = {
-  NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
-int duration = 500;  // 500 miliseconds
- 
-void setup() {
- 
-}
- 
-void loop() {  
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
-    // pin8 output the voice, every scale is 0.5 sencond
-    tone(12, melody[thisNote], duration);
-     
-    // Output the voice after several minutes
-    delay(1000);
-  }
-   
-  // restart after two seconds 
-  delay(2000);
-}
- */
